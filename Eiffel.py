7ca24cd5ab2b21f92f4e7bbf42b686bf3e096596
Thomas Beale <thomas.beale@oceaninformatics.com>
2007-05-10 23:50:13 +0000

    Merge changes from eiffel6 branch rev 230 - 231 into TRUNK

--- Eiffel.py
+++ Eiffel.py
@@ -99,7 +99,7 @@ def ec_emitter(target, source, env):
 		else:
 			project_dir = exe
 
-		project_dir = os.path.dirname(str(source[0])) + '/EIFGENs/' + os.path.splitext(project_dir)[0]
+		project_dir = os.path.abspath(os.path.dirname(str(source[0]))) + '/EIFGENs/' + os.path.splitext(project_dir)[0]
 		exe = project_dir + '/?_code/' + exe
 		result = [project_dir + '/project.epr', exe.replace('?', 'W')]
 
