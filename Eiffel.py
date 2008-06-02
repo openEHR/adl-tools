c47137b898487085eaf3092458cf82939b7c4db4
Peter Gummer <peter.gummer@oceaninformatics.com>
2008-06-02 08:25:59 +0000

    The SCons Eiffel builder's scanner was not detecting .e files in clusters that were specified via an environment variable. To fix this, the common EiffelStudio environment variables (ISE_C_COMPILER, etc.) are now set as construction variables, which can be redefined in the usual manner from the command line, etc.; and the scanner performs construction variable expansion prior to using the paths that it has read from the .ecf file. The scanner also detects more than just .e files: .ecf libraries, .NET assemblies, and external objects and includes.

--- Eiffel.py
+++ Eiffel.py
@@ -114,37 +114,57 @@ def ec_emitter(target, source, env):
 
 	return result
 
-def ecf_target(target, source = None, env = None):
-	"""The ECF target corresponding to the given build target."""
-	return os.path.basename(os.path.dirname(os.path.dirname(str(target[0]))))
-
 def ecf_scanner(node, env, path):
-	"""All Eiffel class files in all clusters mentioned in an ECF file."""
+	"""
+	All dependencies mentioned in 'node', which is expected to be an ECF file.
+	The dependencies consist of:
+	 * All Eiffel class files found in all clusters (including override clusters) mentioned in the ECF file.
+	 * All .ecf library files mentioned in the ECF file. (Such libraries are not themselves scanned).
+	 * All .NET assemblies mentioned in the ECF file.
+	 * All external object files mentioned in the ECF file.
+	 * All .h and .hpp files found in external include directories mentioned in the ECF file.
+	Because this ignores targets and conditionals in the ECF file, it may cause unnecessary builds.
+	"""
 	result = []
 	previous_cluster = ''
 	ecf = xml.dom.minidom.parse(open(str(node)))
 
-	for cluster in ecf.getElementsByTagName("cluster") + ecf.getElementsByTagName("override"):
-		location = cluster.attributes["location"].value.replace('\\', '/')
+	for tag in ['cluster', 'override', 'library', 'assembly', 'external_include', 'external_object']:
+		for element in ecf.getElementsByTagName(tag):
+			location = env.subst(element.attributes['location'].value.replace('$(', '${').replace(')', '}')).replace('\\', '/')
 
-		if location.startswith('$|'):
-			location = os.path.join(previous_cluster, location.replace('$|', '', 1))
-		else:
-			location = previous_cluster = os.path.abspath(os.path.join(os.path.dirname(str(node)), location))
+			if location.startswith('$|'):
+				location = os.path.join(previous_cluster, location.replace('$|', '', 1))
+			elif not os.path.isabs(location):
+				location = os.path.abspath(os.path.join(os.path.dirname(str(node)), location))
 
-		if cluster.attributes.get("recursive", None):
-			result += eiffel_classes_in_cluster(location)
-		else:
-			result += files(location + '/*.e')
+			previous_cluster = location
+
+			if os.path.isfile(location):
+				result += [location]
+			elif tag == 'external_include':
+				result += files(location + '/*.h') + files(location + '/*.hpp')
+			elif element.attributes.get('recursive', None):
+				result += eiffel_classes_in_cluster(location)
+			else:
+				result += files(location + '/*.e')
 
 	return result
+
+def ecf_target(target, source = None, env = None):
+	"""The ECF target corresponding to the given build target."""
+	return os.path.basename(os.path.dirname(os.path.dirname(str(target[0]))))
 
 def generate(env):
 	"""Add a Builder and options for Eiffel to the given Environment."""
 	opts = Options()
-	opts.Add('EC', 'The Eiffel command-line compiler.', 'ec')
-	opts.Add('ECFLAGS', 'Use ec -help to see possible options.', '-finalize -clean')
-	opts.Add('ECLOG', 'File to log Eiffel compiler output.', 'SCons.Eiffel.log')
+	opts.Add('EC', "The Eiffel command-line compiler.", 'ec')
+	opts.Add('ECFLAGS', "Use ec -help to see possible options.", '-finalize -clean')
+	opts.Add('ECLOG', "File to log Eiffel compiler output.", 'SCons.Eiffel.log')
+	opts.Add('ISE_C_COMPILER', "EiffelStudio's C compiler: msc, gcc, mingw, etc.", ise_c_compiler(env))
+	opts.Add('ISE_EIFFEL', "EiffelStudio's installation path.", ise_eiffel(env))
+	opts.Add('ISE_LIBRARY', "EiffelStudio's library path.", ise_library(env))
+	opts.Add('ISE_PLATFORM', "EiffelStudio's platform variable: windows, etc.", ise_platform(env))
 	opts.Update(env)
 	Help(opts.GenerateHelpText(env))
 
@@ -154,6 +174,42 @@ def generate(env):
 def exists(env):
 	"""Is the Eiffel compiler available?"""
 	return env.Detect(env['EC'])
+
+def ise_c_compiler(env):
+	"""The given Environment's ISE_C_COMPILER variable, else a platform-specific assumption."""
+	if env['ENV'].has_key('ISE_C_COMPILER'):
+		return env['ENV']['ISE_C_COMPILER']
+	elif env['PLATFORM'] == 'win32':
+		return 'msc'
+	else:
+		return 'gcc'
+
+def ise_eiffel(env):
+	"""The given Environment's ISE_EIFFEL variable, else computed from the path to the "ec" compiler."""
+	if env['ENV'].has_key('ISE_EIFFEL'):
+		return env['ENV']['ISE_EIFFEL']
+	elif env.WhereIs('ec'):
+		return os.path.abspath(env.WhereIs('ec') + '/../../../../..')
+	else:
+		return ''
+
+def ise_library(env):
+	"""The given Environment's ISE_LIBRARY variable, else the same as ise_eiffel()."""
+	if env['ENV'].has_key('ISE_LIBRARY'):
+		return env['ENV']['ISE_LIBRARY']
+	else:
+		return ise_eiffel(env)
+
+def ise_platform(env):
+	"""The given Environment's ISE_PLATFORM variable, else a platform-specific assumption."""
+	if env['ENV'].has_key('ISE_PLATFORM'):
+		return env['ENV']['ISE_PLATFORM']
+	elif env['PLATFORM'] == 'win32':
+		return 'windows'
+	elif env['PLATFORM'] == 'darwin':
+		return 'macosx-x86'
+	else:
+		return 'linux-x86'
 
 # Utility functions.
 
