72bb89a3eb26cef73753e7075baa79bcc0501ab0
Peter Gummer <peter.gummer@oceaninformatics.com>
2010-08-12 12:04:12 +0000

    Minor formatting fix.

--- apps/adl_workbench/install/mac_osx/ADL_Workbench/ADL Workbench.app/Contents/MacOS/launcher.sh
+++ apps/adl_workbench/install/mac_osx/ADL_Workbench/ADL Workbench.app/Contents/MacOS/launcher.sh
@@ -17,7 +17,7 @@ if [ `uname -r | sed 's/\..*//'` == 8 ]; then
 
 	if [ -z "`ps -wx -ocommand | grep -e '[X]11'`" -a ! -f ~/.xinitrc ]; then
 		echo 'Opening X11 directly, with modified copy of xinitrc to prevent redundant xterm window.'
-        echo "rm -f ~/.xinitrc" > ~/.xinitrc
+		echo "rm -f ~/.xinitrc" > ~/.xinitrc
 		sed 's/xterm/# xterm/' /usr/X11R6/lib/X11/xinit/xinitrc >> ~/.xinitrc
 		open -a X11
 	fi
