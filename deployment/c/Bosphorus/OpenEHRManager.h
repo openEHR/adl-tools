ca19b965c90a71e5258a8d1b07e6e4c651a3b456
Seref Arikan <seref.arikan@oceaninformatics.com>
2010-10-29 15:29:00 +0000

    Bosphorus directory contains a vc++ 2010 Express solution. On windows, mingw ended up being a dead end for implementing c++ callback from Eiffel, so switch to a MS compiler became inevitable. The eclipse project should still stay tough, for its configuration may be of use for linux, and linux version will be using Eclipse CDT anyway. Also in the future Eiffel studio may support mingw properly for c++ callbacks.

---
+++ deployment/c/Bosphorus/OpenEHRManager.h
@@ -0,0 +1,37 @@
+/*
+ * OpenEHRManager.h
+ *
+ *  Created on: 28 Aug 2010
+ *      Author: sarikan
+ */
+
+#ifndef OPENEHRMANAGER_H_
+#define OPENEHRMANAGER_H_
+#include <string>
+#include <vector>
+#include "eif_eiffel.h" /* Exported functions of the Eiffel run-time */
+#include <iostream>
+
+using namespace std;
+
+class OpenEHRManager {
+public:
+	OpenEHRManager();
+	virtual ~OpenEHRManager();
+	void printAttributeValue();
+	static EIF_OBJECT eiffelObj;
+	static EIF_TYPE_ID tid;
+	void printFunctionCallResult();
+	void performParsing();
+	void callMake();
+	vector<string>* getArchetyepNames();
+	void setErrorDBDirPath(string&);
+	void setRmSchemaDirPath(string&);
+	void compileArchetype(string&);
+	int getFunctionCallResult();
+
+private:
+
+};
+
+#endif /* OPENEHRMANAGER_H_ */
