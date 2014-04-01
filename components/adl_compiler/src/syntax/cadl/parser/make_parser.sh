2ccb03e33085bab20baa11395c050bcba9ff3808
wolandscat <thomas.beale@oceaninformatics.com>
2014-04-01 11:27:23 +0100

    Update Gobo geyacc invocations to include rescue clause generation, which is now optional

--- components/adl_compiler/src/syntax/cadl/parser/make_parser.sh
+++ components/adl_compiler/src/syntax/cadl/parser/make_parser.sh
@@ -9,8 +9,8 @@
 #	license:     "Apache 2.0 License <http://www.apache.org/licenses/LICENSE-2.0.html>"
 #
 
-geyacc --new_typing -v cadl_parser_14_errs.txt -t CADL_14_TOKENS -o cadl_14_parser.e cadl_14_parser.y
+geyacc --new_typing --rescue-on-abort -v cadl_parser_14_errs.txt -t CADL_14_TOKENS -o cadl_14_parser.e cadl_14_parser.y
 geyacc --doc=html -v cadl_parser_14_errs.txt -t CADL_14_TOKENS -o cadl_14_parser.html cadl_14_parser.y
 
-geyacc --new_typing -v cadl_parser_15_errs.txt -t CADL_15_TOKENS -o cadl_15_parser.e cadl_15_parser.y
+geyacc --new_typing --rescue-on-abort -v cadl_parser_15_errs.txt -t CADL_15_TOKENS -o cadl_15_parser.e cadl_15_parser.y
 geyacc --doc=html -v cadl_parser_15_errs.txt -t CADL_15_TOKENS -o cadl_15_parser.html cadl_15_parser.y
