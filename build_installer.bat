8838e8e46add12c761fdf553aebdac2f2b881ad8
wolandscat <thomas.beale@openehr.org>
2017-06-30 11:59:15 +0100

    Better versions of Windows build scripts to allow a one-off set_build_env invocation to be made then repeated build_installer invocations.

--- build_installer.bat
+++ build_installer.bat
@@ -1,10 +1,4 @@
-set PYPATH=C:\Program Files (x86)\python2.7
-set PLATFORM=win32
-set ISE_C_COMPILER=mingw
-
-set PATH=c:\program files\nsis;C:\Program Files\Git\cmd;C:\Program Files\Git\bin;C:\Program Files (x86)\NSIS;%PATH%
-set PATH=%PYPATH%;%PYPATH%\Scripts;%PATH%
-
+del /S /Q EIFGENs
 scons ..\downloads\windows\adl_workbench
 
 
---
+++ set_build_env.bat
@@ -0,0 +1,7 @@
+set PYPATH=C:\Program Files (x86)\python2.7
+set PLATFORM=win32
+set ISE_C_COMPILER=mingw
+
+set PATH=c:\program files\nsis;C:\Program Files\Git\cmd;C:\Program Files\Git\bin;C:\Program Files (x86)\NSIS;%PATH%
+set PATH=%ISE_EIFFEL%\studio\spec\win64\bin;%PATH%
+set PATH=%PYPATH%;%PYPATH%\Scripts;%PATH%
