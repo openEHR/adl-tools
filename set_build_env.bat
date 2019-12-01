4381612ef5f7bc0e951a02ef3862b0805a8d9261
wolandscat <thomas.beale@openehr.org>
2019-12-01 17:14:59 +0000

    Update env setting file to read $EIFFEL_HOME from the environment.

--- set_build_env.bat
+++ set_build_env.bat
@@ -1,6 +1,8 @@
 set PYPATH=C:\Program Files (x86)\python2.7
 set PLATFORM=win32
 set ISE_C_COMPILER=mingw
+set ISE_EIFFEL=%EIFFEL_HOME%
+set ISE_LIBRARY=%EIFFEL_HOME%
 
 set PATH=c:\program files\nsis;C:\Program Files\Git\cmd;C:\Program Files\Git\bin;C:\Program Files (x86)\NSIS;%PATH%
 set PATH=%ISE_EIFFEL%\studio\spec\win64\bin;%PATH%
