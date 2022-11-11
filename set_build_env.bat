5f7b1d7bd43c6759c4c3b20ca7d1506201b7c33a
wolandscat <thomas.beale@openehr.org>
2022-11-11 07:49:39 +0000

    Update env vars file

--- set_build_env.bat
+++ set_build_env.bat
@@ -2,8 +2,8 @@ set PYPATH=C:\Program Files\python38
 set PLATFORM=win64
 set ISE_C_COMPILER=mingw
 
-set ISE_EIFFEL=C:\Program Files\Eiffel Software\EiffelStudio 19.05 GPL
+set ISE_EIFFEL=C:\Program Files\Eiffel Software\EiffelStudio 22.05 Standard
 set ISE_LIBRARY=%ISE_EIFFEL%
 
-set PATH=C:\Program Files (x86)\NSIS\bin;%PATH%
+set PATH=C:\Program Files (x86)\NSIS\Bin;%PATH%
 set PATH=%ISE_EIFFEL%\studio\spec\%PLATFORM%\bin;%PATH%
