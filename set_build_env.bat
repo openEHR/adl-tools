f662f346b7d0dd30e673d533f14c6c4b3183703c
wolandscat <thomas.beale@graphitehealth.io>
2024-01-17 23:13:33 -0700

    Update build files.

--- set_build_env.bat
+++ set_build_env.bat
@@ -2,7 +2,7 @@ set PYPATH=C:\Program Files\python38
 set PLATFORM=win64
 set ISE_C_COMPILER=mingw
 
-set ISE_EIFFEL=C:\Program Files\Eiffel Software\EiffelStudio 22.05 Standard
+set ISE_EIFFEL=C:\Program Files\Eiffel Software\EiffelStudio 23.09 Standard
 set ISE_LIBRARY=%ISE_EIFFEL%
 
 set PATH=C:\Program Files (x86)\NSIS\Bin;%PATH%
