ac1346282d6e5d496bb340e1836270c956a78e16
wolandscat <thomas.beale@graphitehealth.io>
2024-06-27 18:42:55 -0600

    Use MSC compiler

--- set_build_env.bat
+++ set_build_env.bat
@@ -1,6 +1,6 @@
-set PYPATH=C:\Program Files\python38
+set PYPATH=C:\Program Files\Python312
 set PLATFORM=win64
-set ISE_C_COMPILER=mingw
+set ISE_C_COMPILER=msc_vc140
 
 set ISE_EIFFEL=C:\Program Files\Eiffel Software\EiffelStudio 23.09 Standard
 set ISE_LIBRARY=%ISE_EIFFEL%
